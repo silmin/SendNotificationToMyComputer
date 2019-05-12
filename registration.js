01df270051155c5da9e21bf19da2f77b752fc48c
silmin <silvestmint@gmail.com>
2019-05-12 13:46:16 +0900

    add: get various keys

--- registration.js
+++ registration.js
@@ -6,5 +6,22 @@ window.addEventListener('load', function() {
             }).catch(function(error) {
                 console.log("serviceWorker error.", error);
             });
+
+        navigator.serviceWorker.ready
+            .then(function (registration) {
+                return registration.pushManager.subscribe({ userVisibleOnly: true; });
+            })
+            .then(function (subscription) {
+                console.log('FCM EndPoint is: ' + subscription.endpoint);
+                var auth = subscription.getKey('auth') 
+                    ? btoa(String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('auth'))))
+                    : '';
+                console.log('User Auth is: ' + auth);
+                var publicKey = subscription.getKey('p256dh')
+                    ? btoa(String.fromCharCode.apply(null, new Uint8Array(subscription.getKey('p256dh'))))
+                    : '';
+                console.log('User PublicKey is: ' + publicKey);
+            })
+            .catch(console.error.bind(console));
     }
 });
