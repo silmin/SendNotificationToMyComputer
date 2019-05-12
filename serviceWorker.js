800aa5f86ea30e4a1e4293b32c78870259366275
silmin <silvestmint@gmail.com>
2019-05-12 14:15:36 +0900

    add: event 'fetch', 'push', 'notificationclick'

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,2 +1,25 @@
 self.addEventListener('fetch', function(event)) {
+    event.respondWith(
+        new Response('[active] service worker')
+    );
 });
+
+self.addEventListener('push', function(event)) {
+    console.log('Received a push message', event);
+    
+    var title = "push title";
+    var body = "push body";
+
+    event.waitUntill(
+        self.registration.showNotification(title, {
+            body: body,
+            icon: './img/icon.png',
+            tag: 'push-notification-tag'
+        });
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    clients.openWindow("./");
+}, false);
