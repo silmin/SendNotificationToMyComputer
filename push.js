ea373235fd82011c144510db2089770c034c2fd1
silmin_ <silvestmint@gmail.com>
2019-05-13 13:52:01 +0000

    fix: use vapid

--- push.js
+++ push.js
@@ -1,30 +1,29 @@
 'use strict';
 
-const push = require('web-push');
+const webpush = require('web-push');
 
-const GCM_API_KEY = 'AIzaSyDDTUZzzAUqX3dg0J2Rong3IK0s9pyBRj4';
-push.setGCMAPIKey(GCM_API_KEY);
+const keys = require('./applicatin-server-keys.json');
+webpush.setVapidDetails(
+    "mailto:silvestmint@gmail.com",
+    keys.publicKey,
+    keys.privateKey
+);
 
-const data = {
-    'endpoint': 'https://fcm.googleapis.com/fcm/send/dQlqh1Fq6HY:APA91bHKQWmhaN0f-DIgyUJz2XPXlEkbRCaRSW_mmO1kIQmh8qh6tB5n5PoVM7q97FDMeMrNSA1OwLes7T3K9M4Y0hlINdeEBIgWRyRIBP9ZYo2cyMoCI21jOS_lfMVbOHlh9Ljs2OVC',
-    'userAuth': '7aI+EM+f2xo8Tln9CqbXoQ==',
-    'userPublicKey': 'BBcKSr4mmSgJc/BFIN3RtlCAtSm+7gewje/vEgxqheSpToiqwEatAMKFTIrQIBEZUHTtQ2DJt9f+cKGs7PJz+50='
+const subscription = require('./subscription.json');
+
+const icon = './img/icon.png';
+const params = {
+    title: 'This is Push!',
+    msg: 'This is msg,
+    icon: icon
 };
 
-const pushSubscription = {
-    pendpoint: data.endpoint,
-    keys: {
-        auth: data.userAuth,
-        p256dh: data.userPublicKey
-    }
-};
-
-push.sendNotification(pushSubscription, "Hi! I'm push.js")
-    .then(function(result) {
-        console.log("Success!");
-        console.log(result);
-    })
-    .catch(function(err) {
-        console.log("Fail!");
-        console.error(err);
-    });
+push.sendNotification(subscription, "Hi! I'm push.js")
+.then(res => {
+    console.log("Success!");
+    console.log(res);
+})
+.catch(err => {
+    console.log("Fail!");
+    console.error(err);
+});
