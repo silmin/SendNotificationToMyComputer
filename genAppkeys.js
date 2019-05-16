fac7acbecaa3adde35592ccf1d4c7b4cf6eb39c5
silmin_ <silvestmint@gmail.com>
2019-05-16 13:35:37 +0000

    del: cached

--- genAppkeys.js
+++
@@ -1,5 +0,0 @@
-const webpush = require("web-push");
-const fs = require('fs');
-
-var keys_json = JSON.stringify(webpush.generateVAPIDKeys());
-fs.writeFile('application-server-keys.json', keys_json);
